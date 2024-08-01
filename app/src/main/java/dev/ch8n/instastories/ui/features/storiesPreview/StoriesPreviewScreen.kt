f69aa72b129be6b2800a71ec4438218144ee9b26
ch8n <chetan.garg36@gmail.com>
2024-08-01 23:51:41 +0530

    cross icon added

--- app/src/main/java/dev/ch8n/instastories/ui/features/storiesPreview/StoriesPreviewScreen.kt
+++ app/src/main/java/dev/ch8n/instastories/ui/features/storiesPreview/StoriesPreviewScreen.kt
@@ -3,7 +3,9 @@ package dev.ch8n.instastories.ui.features.storiesPreview
 import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -20,7 +22,9 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -33,21 +37,25 @@ import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import dev.ch8n.instastories.R
 import dev.ch8n.instastories.data.remote.config.RemoteServiceProvider
 import dev.ch8n.instastories.data.repositories.StoryRepository
 import dev.ch8n.instastories.domain.usecases.GetStoriesRemoteUseCase
 import dev.ch8n.instastories.ui.features.storiesPreview.component.AutoScrollPagerIndicator
 import dev.ch8n.instastories.ui.features.storiesPreview.component.AutoScrollingPager
+import dev.ch8n.instastories.ui.features.storiesPreview.component.CrossIcon
 import dev.ch8n.instastories.utils.randomColor
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -81,6 +89,7 @@ fun StoriesPreviewScreen(
     ) {
         if (storiesHomeState.stories.isNotEmpty()) {
 
+
             val pagerState = rememberPagerState(
                 initialPage = 0,
                 pageCount = { storiesHomeState.stories.size }
@@ -106,6 +115,16 @@ fun StoriesPreviewScreen(
                 pagerState = pagerState,
                 indicatorWidth = 45.dp,
                 indicatorCount = storiesHomeState.stories.size
+            )
+
+            CrossIcon(
+                modifier = Modifier
+                    .size(56.dp)
+                    .padding(16.dp)
+                    .align(Alignment.TopEnd),
+                onClick = {
+
+                },
             )
         }
     }
---
+++ app/src/main/java/dev/ch8n/instastories/ui/features/storiesPreview/component/CrossIcon.kt
@@ -0,0 +1,33 @@
+package dev.ch8n.instastories.ui.features.storiesPreview.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import dev.ch8n.instastories.R
+
+@Composable
+fun CrossIcon(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_close_24),
+        contentDescription = null,
+        modifier = modifier
+            .clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() },
+                onClick = onClick
+            ),
+        tint = Color.Gray
+    )
+}
