27dfd7dbea0a6988630a330478c9b7a50b05e535
ch8n <chetan.garg36@gmail.com>
2024-08-02 14:36:10 +0530

    removed roomdb as not required and added image caching

--- app/src/main/java/dev/ch8n/instastories/domain/injector/RepositoryProvider.kt
+++ app/src/main/java/dev/ch8n/instastories/domain/injector/RepositoryProvider.kt
@@ -1,6 +1,5 @@
 package dev.ch8n.instastories.domain.injector
 
-import dev.ch8n.instastories.data.local.injector.LocalServiceProvider
 import dev.ch8n.instastories.data.remote.injector.RemoteServiceProvider
 import dev.ch8n.instastories.data.repositories.StoryRepository
 
@@ -9,7 +8,6 @@ object RepositoryProvider {
     val storyRepository by lazy {
         StoryRepository(
             RemoteServiceProvider.storiesService,
-            LocalServiceProvider.viewStoriesDAO
         )
     }
 }
