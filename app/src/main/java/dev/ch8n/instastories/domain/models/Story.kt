721386ecc9ca36e69c7ddb7eb63f91f920e982ac
ch8n <chetan.garg36@gmail.com>
2024-08-02 14:35:38 +0530

    removed roomdb as not required and added image caching

--- app/src/main/java/dev/ch8n/instastories/domain/models/Story.kt
+++ app/src/main/java/dev/ch8n/instastories/domain/models/Story.kt
@@ -1,19 +1,7 @@
 package dev.ch8n.instastories.domain.models
 
-import dev.ch8n.instastories.data.local.entities.StoryEntity
-
 data class Story(
     val id: String,
     val imageUrl: String,
     val userName: String
-) {
-    companion object {
-        fun Story.toEntity(): StoryEntity {
-            return StoryEntity(
-                id = id,
-                imageUrl = imageUrl,
-                userName = userName
-            )
-        }
-    }
-}
+)
